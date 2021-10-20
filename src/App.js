73f68bf5f4be5177fb6068b592220f30c2d52ec9
Benjamin Martinez <benjamin.martiinez@gmail.com>
2021-10-20 16:28:09 -0400

    Created app skeleton.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" />
+        <Route path="/search" />
+      </Switch>
+      <div>
+        App Placeholder
+      </div>
+    </Router>
   );
 }
 
