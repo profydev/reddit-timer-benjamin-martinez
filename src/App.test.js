879fa05ff846a2f4bea78962a481aab3ed8e5fb8
Benjamin Martinez <benjamin.martiinez@gmail.com>
2021-10-21 16:34:44 -0400

    Fixed linting error 2?

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
-import App from './App';
-import React from "react"
+import React from 'react';
 import userEvent from '@testing-library/user-event';
+import App from './App';
 
 function setup() {
   return render(
@@ -12,29 +12,24 @@ function setup() {
   );
 }
 
-//Header test
+//Header test 
 describe('Header', () => {
   test('Logo link points to the correct page', () => {
     setup();
-    
     const link = screen.getByRole('link', { name: /logo.svg/i });
     screen.debug(link);
     userEvent.click(link);
-    
     expect(
       screen.getByRole('heading', { name: /Home/i })
     ).toBeInTheDocument();
   });
 
   test('"Search" link points to the correct page', () => {
-    setup()
-    
+    setup();
     const link = screen.getByRole('link', { name: /Search/i });
     userEvent.click(link);
-    
     expect(
       screen.getByRole('heading', { name: /Search/i })
     ).toBeInTheDocument();
   });
-
 });
