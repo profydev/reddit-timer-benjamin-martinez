d02e7785161ba76aefd1c63d299a39fd0cc727f9
Benjamin Martinez <benjamin.martiinez@gmail.com>
2021-10-25 17:28:22 -0400

    Corrections made to homepage test

--- src/Home.test.js
+++ src/Home.test.js
@@ -3,26 +3,29 @@ import { MemoryRouter } from 'react-router';
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 import Home from './pages/Home';
+import Search from './pages/Search';
 
 function setup() {
   return render(
     <MemoryRouter>
       <Home />
+      <Search />
     </MemoryRouter>,
   );
 }
 
 describe('Hero Section', () => {
-  test('"Show me the best time" button points to the correct page', () => {
-    setup();
-    const link = screen.getByText(/Show me the best time/i);
-    screen.debug(link);
-    userEvent.click(link);
-    expect(
-      screen.getByRole('heading', { name: /Search/i }),
-    ).toBeInTheDocument();
+    test('"Show me the best time" button points to the correct page', () => {
+      setup();
+      const link = screen.getByRole('link', { name: /Show me the best time/i });
+      screen.debug(link);
+      userEvent.click(link);
+      screen.debug();
+      expect(
+        screen.getByText('Search'),
+      ).toBeInTheDocument();
+    });
   });
-});
 
 describe('Info Section', () => {
   test('"Profy.dev" link points to the correct page', () => {
