d1c7d60a1df48cb7d5d1ae112d1e69c23e312fe7
Benjamin Martinez <benjamin.martiinez@gmail.com>
2021-10-25 16:57:34 -0400

    Implemented Info section as specified by Figma design

--- src/Home.test.js
+++ src/Home.test.js
@@ -2,12 +2,12 @@ import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import React from 'react';
 import userEvent from '@testing-library/user-event';
-import App from './App';
+import Home from './pages/Home';
 
 function setup() {
   return render(
     <MemoryRouter>
-      <App />
+      <Home />
     </MemoryRouter>,
   );
 }
@@ -15,11 +15,23 @@ function setup() {
 describe('Hero Section', () => {
   test('"Show me the best time" button points to the correct page', () => {
     setup();
-    const link = screen.getByRole('link', { name: /Show me the best time/i });
+    const link = screen.getByText(/Show me the best time/i);
     screen.debug(link);
     userEvent.click(link);
     expect(
       screen.getByRole('heading', { name: /Search/i }),
     ).toBeInTheDocument();
+  });
+});
+
+describe('Info Section', () => {
+  test('"Profy.dev" link points to the correct page', () => {
+    setup();
+    expect(screen.getByText(/profy.dev/i).closest('a')).toHaveAttribute('href', 'https://profy.dev');
+  });
+
+  test('"Click here for more information" link points to the correct page', () => {
+    setup();
+    expect(screen.getByText(/Click here for more information/i).closest('a')).toHaveAttribute('href', 'https://profy.dev/employers');
   });
 });
