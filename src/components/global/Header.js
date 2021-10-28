eae1c6d69f0e69911715d2f5407580f0857ec632
Benjamin Martinez <benjamin.martiinez@gmail.com>
2021-10-28 00:25:27 -0400

    Changed Search link in header to push router

--- src/components/global/Header.js
+++ src/components/global/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom'
 
 export default function Header() {
+  const history = useHistory();
+
+  function handleClick(e) {
+    history.push(`/search/javascript`);
+  }
+
   return (
     <Wrapper>
       <Link to="/"><Logo src="logo.svg" alt="logo.svg" /></Link>
       <Navigation>
-        <Link to="/search/javascript">Search</Link>
+        <Link to="/search/javascript" onClick={handleClick}>Search</Link>
         <NavLink href="#how-it-works">How it works</NavLink>
         <NavLink href="#about">About</NavLink>
       </Navigation>
